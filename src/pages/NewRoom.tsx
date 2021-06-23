e552fcb736dcb3965639a1bfa25375b48af6f33b
MatheusPatriota <mpatriota55@gmail.com>
2021-06-22 21:12:45 -0300

    segundo dia do NLW concluido, hooks, auth, pagina incial e new Room

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import "../styles/auth.scss";
+
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export function NewRoom() {
+  const {user} = useAuth();
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie Salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real </p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="LetMeAsk" />
+          <h2>Criar uma Nova Sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da Sala" />
+            <Button type="submit">Entrar na Sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
